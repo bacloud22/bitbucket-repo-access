81175fc2a0403ea565edebe16ad0a27cdf870398
open-listings <open-listings@gmx.com>
2025-02-21 08:39:34 +0100

    sog lekhmiss

--- api/add-user.js
+++ api/add-user.js
@@ -1,79 +1,68 @@
-const {
-  Bitbucket
-} = require('bitbucket')
+// const { Bitbucket } = require("bitbucket");
 const Envato = require("envato");
 const repomap = require("./repomap.js");
-const bitbucket_token = process.env.BITBUCKET_KEY;
+const nodemailer = require("nodemailer");
+
+// const bitbucket_token = process.env.BITBUCKET_KEY;
 const envato_token = process.env.ENVATO_KEY;
-// const envato = new Envato.Client(envato_token);
+// const MAILTRAP_TOKEN = process.env.MAILTRAP_TOKEN;
 
-const clientOptions = {
-  auth: {
-      token: bitbucket_token,
-  },
-}
+const USERNAME = process.env.USERNAME;
+const PASSWORD = process.env.PASSWORD;
 
-const bitbucket = new Bitbucket(clientOptions)
+const envato = new Envato.Client(envato_token);
+
+// const clientOptions = {
+//   auth: {
+//     username: USERNAME,
+//     password: PASSWORD,
+//   },
+// };
+
+// Looking to send emails in production? Check out our Email API/SMTP product!
+var transport = nodemailer.createTransport({
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: USERNAME,
+      pass: PASSWORD
+    }
+  });
+
+const sender = {
+  address: "hello@example.com",
+  name: "ENVATO $$$$",
+};
+const recipients = ["contact@open-listings.net"];
+
+// const bitbucket = new Bitbucket(clientOptions);
 
 async function addUSerToRepo(req, res) {
   const data = JSON.parse(req.body);
 
   try {
-    //   const sale = await envato.private.getSale(data.purchase_code);
-
-    //   const itemID = sale.item.id;
-    //   const reponame = repomap[itemID];
-      if (true) {
-          bitbucket.repositories.updateUserPermission({
-                  workspace: 'open-listings',
-                  repo_slug: 'open-listings-pro',
-                  selected_user_id: data.username,
-                  _body: {
-                      permission: 'read'
-                  }
-              })
-              .then((gitdata) => {
-
-                  if (gitdata.status == 201) {
-                      res.status(200).send({
-                          status: 200,
-                          message: "Succesfully Invited. Please check your email",
-                      });
-                  } else if (gitdata.status == 204) {
-                      res.status(200).send({
-                          status: 204,
-                          message: "User is already a Collaborator",
-                      });
-                  }
-              })
-              .catch((err) => {
-                  console.log(err);
-                  res.status(400).send({
-                      status: 400,
-                      message: "Invite Failed. Please check your Username",
-                  });
-              });
-      } else if (itemID) {
-          res.status(400).send({
-              status: 400,
-              message: "This Template Repo is not available in Github",
-          });
-      } else {
-          res
-              .status(400)
-              .send({
-                  status: 400,
-                  message: "Cannot validate your Purchase"
-              });
-      }
+    const sale = await envato.private.getSale(data.purchase_code);
+    const itemID = sale.item.id;
+    const reponame = repomap[itemID];
+    if (reponame) {
+      // TODO: enrich repomap
+      await transport
+        .sendMail({
+          from: sender,
+          to: recipients,
+          subject: "ENVATO $$",
+          text: `Congrats: Username: ${data.username} purchased ${itemID}`, //
+          category: "Integration Test",
+          sandbox: true,
+        })
+        .then(console.log, console.error);
+    }
   } catch (error) {
-      console.log("envato fetch error:", error);
-      res
-          .status(400)
-          .send({
-              status: 400,
-              message: "Could not verify Purchase Code"
-          });
+    console.log("envato fetch error:", error);
+    res.status(400).send({
+      status: 400,
+      message: "Could not verify Purchase Code",
+    });
   }
 }
 
